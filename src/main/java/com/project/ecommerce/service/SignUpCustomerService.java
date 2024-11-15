b306f969f35e69d2de6884ea2bcadb263f34071b
heeejinin <khin0115@gmail.com>
2024-11-15 21:27:09 +0900

    [Email-Verification] 커스터머 이메일 verification

--- src/main/java/com/project/ecommerce/service/SignUpCustomerService.java
+++ src/main/java/com/project/ecommerce/service/SignUpCustomerService.java
@@ -3,8 +3,15 @@ package com.project.ecommerce.service;
 import com.project.ecommerce.domain.SignUpForm;
 import com.project.ecommerce.domain.model.Customer;
 import com.project.ecommerce.domain.repository.CustomerRepository;
+import com.project.ecommerce.exception.CustomException;
+import com.project.ecommerce.exception.ErrorCode.ErrorCode;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.time.LocalDateTime;
+import java.util.Locale;
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
@@ -14,5 +21,42 @@ public class SignUpCustomerService {
 
     public Customer signUp(SignUpForm form) {
         return customerRepository.save(Customer.from(form));
+    }
+
+    // 이메일 validation - 이메일 존재 시 true 반환
+    public boolean isEmailExist(String email) {
+        return customerRepository.findByEmail(email.toLowerCase(Locale.ROOT)).isPresent(); // 소문자 변환
+    }
+
+    // 이메일 검증 메소드
+    @Transactional
+    public void verifyEmail(String email, String code) {
+        Customer customer = customerRepository.findByEmail(email)
+                .orElseThrow(()->new CustomException(ErrorCode.NOT_FOUND_USER));
+        if (customer.isVerify()) {
+            throw new CustomException(ErrorCode.ALREADY_VERIFIED);
+        }
+        if (!customer.getVerificationCode().equals(code)) {
+            throw new CustomException(ErrorCode.WRONG_VERIFICATION);
+        }
+        if (customer.getVerifyExpiredAt().isBefore(LocalDateTime.now())) {
+            throw new CustomException(ErrorCode.EXPIRED_CODE);
+        }
+        customer.setVerify(true);
+    }
+
+    // 커스터마이징한 계정 수정 (언제 만료되는지를 반환)
+    @Transactional // 트랜잭션을 걸게 되면 내부에서 동작하는 것 때문에 실제로 세이브하지 않아도 바뀐게 있으면 알아서 저장하게 됨
+    public LocalDateTime changeCustomerValidateEmail(Long customerId, String verificationCode) {
+        Optional<Customer> customerOptional = customerRepository.findById(customerId);
+
+        if (customerOptional.isPresent()) {
+            Customer customer = customerOptional.get();
+            customer.setVerificationCode(verificationCode);
+            customer.setVerifyExpiredAt(LocalDateTime.now().plusDays(1)); // 하루 뒤 만료
+
+            return customer.getVerifyExpiredAt();
+        }
+        throw new CustomException(ErrorCode.NOT_FOUND_USER);
     }
 }
